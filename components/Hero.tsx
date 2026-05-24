8249b813d79ae1717f95279e3709da7dc9a12f3a
Jblendzzz <cfcn2021@gmail.com>
2026-05-24 13:56:18 +0800

    Swap hero image to client-requested front page photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <section id="top" className="relative isolate overflow-hidden bg-ink">
       <div className="absolute inset-0 -z-10">
         <img
-          src="/gallery/front-page.webp"
+          src="/gallery/front-page.jpg"
           alt="Sharp cut by Jblendzzz"
           className="h-full w-full object-cover opacity-70"
         />
